b5c5a500c5bccafed4ec3606f3996c77f79cc106
Tran Phong <tranphong96.hbk@gmail.com>
2022-01-09 15:08:56 +0800

    delete _site

--- _site/assets/js/simplejs.js
+++
@@ -1,8 +0,0 @@
-function copyURI(evt) {
-    evt.preventDefault();
-    navigator.clipboard.writeText(evt.target.getAttribute('href')).then(() => {
-      /* clipboard successfully set */
-    }, () => {
-      /* clipboard write failed */
-    });
-}
